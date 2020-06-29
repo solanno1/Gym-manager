ca65f89be023bb81e5ee6d036f925ec8afcf1ed5
solanno1 <solano.oliveira@fatec.sp.gov.br>
2020-06-29 10:41:10 -0300

    Configuração de rotas

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const routes = express.Router()
-
+const instructors = require('./instructors')
 
 
 routes.get('/', function(req, res){
@@ -11,9 +11,14 @@ routes.get('/instructors', function(req, res){
     return res.render("instructors/index")
 })
 
+routes.get('/instructors/create', function(req, res){
+    return res.render("instructors/create")
+})
+
 routes.get('/members', function(req, res){
     return res.send("members")
 })
 
+routes.post("/instructors", instructors.post)
 
 module.exports = routes
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const server = express()
 
 const routes = require("./routes")
 
+server.use(express.urlencoded({ extended: true }))
 server.use(express.static('public'))
 
 server.use(routes)
