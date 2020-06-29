16ac796539de12e6af07d9a2d203782124834b7e
solanno1 <solano.oliveira@fatec.sp.gov.br>
2020-06-29 14:30:06 -0300

    Desestruturação de objeto JS

--- instructors.js
+++ instructors.js
@@ -10,11 +10,22 @@ exports.post = function(req, res){
             return res.send("Please, fill all fields")
         }
     }
+    let {avatar_url, birth, name, services, gender} = req.body
+    birth =  Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)
 
-    req.body.birth =  Date.parse(req.body.birth)
-    req.body.created_at = Date.now()
 
-    data.instructors.push(req.body)
+
+    data.instructors.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        gender,
+        services,
+        created_at,
+    })
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
         if(err) return res.send("Write file error")
