eb767f39daf3f09642dfb7fe3a2072874c03599d
solanno1 <solano.oliveira@fatec.sp.gov.br>
2020-07-01 08:25:38 -0300

    JS

--- instructors.js
+++ instructors.js
@@ -1,5 +1,25 @@
 const fs = require('fs')
 const data = require("./data.json")
+const {age} = require("./utils")
+
+exports.show = function(req, res){
+    const { id } = req.params
+    const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id
+    })
+
+    if(!foundInstructor) return res.send("Instructor not found")    
+
+    const instructor = {
+        ...foundInstructor,
+        age: age(foundInstructor.birth),
+        services: foundInstructor.services.split(","),
+        created_at: "",
+    }
+
+    return res.render("instructors/show", {instructor})
+}
+
 // create
 exports.post = function(req, res){
 
@@ -38,3 +58,4 @@ exports.post = function(req, res){
 // update
 
 // delete
+
