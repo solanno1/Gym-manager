615229d17f470ac4d0df07e0cc99ffa25c9e526e
solanno1 <solano.oliveira@fatec.sp.gov.br>
2020-07-08 13:46:13 -0300

    CRUD completo

--- instructors.js
+++ instructors.js
@@ -53,8 +53,6 @@ exports.post = function(req, res){
     const created_at = Date.now()
     const id = Number(data.instructors.length + 1)
 
-
-
     data.instructors.push({
         id,
         avatar_url,
@@ -74,8 +72,46 @@ exports.post = function(req, res){
     // return res.send(req.body)
 }
 
+//put
+exports.put = function(req, res){
+    const { id } = req.body
+    let index = 0
+    const foundInstructor = data.instructors.find(function(instructor, foundIndex){
+        if(id == instructor.id){
+            index = foundIndex
+            return true
+        }
+        
+    })
 
-// update
+    if(!foundInstructor) return res.send("Instructor not found")  
+
+    const instructor = {
+        ...foundInstructor,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.instructors[index] = instructor
+    
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if(err) return res.send("Write error!")
+
+        return res.redirect(`/instructors/${id}`)
+    })
+}
 
 // delete
+exports.delete = function(req, res){
+    const {id} = req.body
+
+    const filteredInstructors = data.instructors.filter(function(instructor){        
+        return instructor.id != id
+    })
+    data.instructors = filteredInstructors
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if(err) return res.send("Write File Error!")
+        return res.redirect("/instructors")
+    })
+}
 
--- routes.js
+++ routes.js
@@ -17,11 +17,15 @@ routes.get('/instructors/create', function(req, res){
 
 routes.get('/instructors/:id', instructors.show)
 
+routes.delete('/instructors', instructors.delete)
+
 routes.get('/members', function(req, res){
     return res.send("members")
 })
 
 routes.get('/instructors/:id/edit', instructors.edit)
+
+routes.put('/instructors', instructors.put)
 
 routes.post("/instructors", instructors.post)
 
